1778a03b5c03bf904547bc4af4adbe970222fb53
Jacob Sayles <jsayles@gmail.com>
2020-03-15 14:38:53 -0700

    now with local settings

--- support/settings.py
+++ support/settings.py
@@ -15,15 +15,14 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jtrc8mzb9yq#q8l@(!s*7d%0e@__+s!ptp5w=ij#@4(@9xw4cq'
+SECRET_KEY = 'PRIVATE';
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -102,15 +101,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
+LANGUAGE_CODE = 'en'
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
@@ -118,3 +112,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Load the local settings file
+if os.path.isfile('support/local_settings.py'):
+    from .local_settings import *
+else:
+    print("No local settings file found")
